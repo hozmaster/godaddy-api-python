912a66be1c098de020b76987eeed41510b5c553f
Olli-Pekka Wallin <olli-pekka.wallin@elfgroup.fi>
2018-06-05 16:01:03 +0300

    Start to get domain ip info from godaddy

--- godaddy_dns.py
+++ godaddy_dns.py
@@ -1,4 +1,4 @@
-import os
+from subprocess import call
 import re
 import sys
 import urllib.request
@@ -9,6 +9,35 @@ class GoDaddyDNSUpdater(object):
 
     def __init__(self, file: str):
         self.settings = json.load(open(file))
+        self.api_key = ""
+        self.secret = ""
+
+    def restart_haproxy(self):
+
+        call(["/usr/local/etc/rc.d/haproxy.sh", "restart"])
+
+    def get_domain_ip_goddy(self, domain, hostname):
+
+        """get IP of A record from godaddy."""
+
+        url = "https://api.godaddy.com/v1/domains/{}/records/A/{}".format(domain, hostname)
+        headers = {'Authorization': 'sso-key {}:{}'.format(self.api_key, self.secret),
+                   'Accept': 'application/json'}
+        # request = urllib.request.Request(url, headers=headers)
+        # # response = urllib.request.urlopen(request)
+        #
+        # if response.getcode() == 200:
+        #     json_data = json.load(response)
+        #     name = json_data[0]['name']
+        #     godaddyIP = json_data[0]['data']
+        #     if name == hostname:
+        #         return godaddyIP
+        #     else:
+        #         print('godaddy hostname: {} does not match searched hostname: {}'.format(name, hostname))
+        # else:
+        #     print(response.getcode())
+        #     print(response.read())
+        pass
 
     def get_public_ip(self):
 
@@ -23,6 +52,16 @@ class GoDaddyDNSUpdater(object):
     def main(self):
 
         public_ip = self.get_public_ip ()
+
+        godaddy = self.settings["godaddy"]
+
+        self.api_key = godaddy['api.key']
+        self.secret = godaddy['api.secret']
+
+        for domain in godaddy['domains']:
+            for hostname in domain['a-records']:
+                self.get_domain_ip_goddy(domain['domain'], hostname)
+
         print(public_ip)
 
 
