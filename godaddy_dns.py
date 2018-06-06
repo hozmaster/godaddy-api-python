47e6702d91139eab6d90c15df6cfa88a7a2c0409
Olli-Pekka Wallin <olli-pekka.wallin@elfgroup.fi>
2018-06-06 15:53:24 +0300

    Change to use http requests

--- godaddy_dns.py
+++ godaddy_dns.py
@@ -1,6 +1,7 @@
 from subprocess import call
 import re
 import sys
+import http.client
 import urllib.request
 import json
 
@@ -16,28 +17,52 @@ class GoDaddyDNSUpdater(object):
 
         call(["/usr/local/etc/rc.d/haproxy.sh", "restart"])
 
-    def get_domain_ip_goddy(self, domain, hostname):
+    def get_domain_info(self, domain):
 
-        """get IP of A record from godaddy."""
+        url = "api.ote-godaddy.com"
+        path = "/v1/domains/available?domain={}".format(domain)
+        connection = http.client.HTTPSConnection(url)
 
-        url = "https://api.godaddy.com/v1/domains/{}/records/A/{}".format(domain, hostname)
         headers = {'Authorization': 'sso-key {}:{}'.format(self.api_key, self.secret),
                    'Accept': 'application/json'}
-        # request = urllib.request.Request(url, headers=headers)
-        # # response = urllib.request.urlopen(request)
-        #
-        # if response.getcode() == 200:
-        #     json_data = json.load(response)
-        #     name = json_data[0]['name']
-        #     godaddyIP = json_data[0]['data']
-        #     if name == hostname:
-        #         return godaddyIP
-        #     else:
-        #         print('godaddy hostname: {} does not match searched hostname: {}'.format(name, hostname))
-        # else:
-        #     print(response.getcode())
-        #     print(response.read())
+
+        connection.request("GET", path, "", headers)
+        response = connection.getresponse()
+        print("Status: {} and reason: {}".format(response.status, response.reason))
+        print(response.read().decode())
+
+        connection.close()
+
         pass
+
+    def get_domain_ip(self, domain):
+        pass
+
+    # def get_domain_ip_goddy(self, domain, hostname):
+    #
+    #     """get IP of A record from godaddy."""
+    #     url = "https://api.ote-godaddy.com/v1/domain/{}".format(domain)
+    #     headers = {'Authorization': 'sso-key {}:{}'.format(self.api_key, self.secret),
+    #                'Accept': 'application/json'}
+    #
+    #     request = urllib.request.Request(url, headers=headers)
+    #
+    #     # response = urllib.request.urlopen(request)
+    #     response = urllib.request.urlopen(request).read()
+    #
+    #     #
+    #     if response.getcode() == 200:
+    #         json_data = json.load(response)
+    #     #     name = json_data[0]['name']
+    #     #     godaddyIP = json_data[0]['data']
+    #     #     if name == hostname:
+    #     #         return godaddyIP
+    #     #     else:
+    #     #         print('godaddy hostname: {} does not match searched hostname: {}'.format(name, hostname))
+    #     else:
+    #          print(response.getcode())
+    #          print(response.read())
+    #     pass
 
     def get_public_ip(self):
 
@@ -60,7 +85,7 @@ class GoDaddyDNSUpdater(object):
 
         for domain in godaddy['domains']:
             for hostname in domain['a-records']:
-                self.get_domain_ip_goddy(domain['domain'], hostname)
+                self.get_domain_info(domain['domain'])
 
         print(public_ip)
 
