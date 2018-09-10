96ef19aafad90d7b81a768857bb7ac0fac9e4e50
Olli-Pekka Wallin <olli-pekka.wallin@elfgroup.fi>
2018-09-10 10:36:41 +0300

    Move script to another place

--- deployment/godaddy-dns.sh
+++ deployment/godaddy-dns.sh
@@ -1,5 +1,18 @@
 #! /bin/bash
-source name_Env/bin/activate
-# virtualenv is now active.
-#
-/usr/bin/python3 go_daddy-dns.py
+
+set -eu
+
+. /opt/godaddy/dns/locking
+
+__prep_lock
+exclusive_lock_now || exit 1
+
+PROJECT_ENV=/opt/godaddy/env
+PROJECT_LOC=PROJECT_ENV/lib/python3.6/site-packages/godaddy-dns
+
+$PROJECT_ENV/bin/python $PROJECT_LOC/dns/dashboard.py %1 >> /var/log/yedor.net/godaddy-dns.log
+
+#source name_Env/bin/activate
+## virtualenv is now active.
+##
+#/usr/bin/python3 godaddy-dns.py
