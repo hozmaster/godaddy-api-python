113fc1f5bc36fc58b18fa809a9c6aeb0115bd6bf
Olli-Pekka Wallin <olli-pekka.wallin@elfgroup.fi>
2018-09-10 10:47:43 +0300

    Fix typo from script

--- deployment/godaddy-dns.sh
+++ deployment/godaddy-dns.sh
@@ -10,7 +10,7 @@ exclusive_lock_now || exit 1
 PROJECT_ENV=/opt/godaddy/env
 PROJECT_LOC=PROJECT_ENV/lib/python3.6/site-packages/godaddy-dns
 
-$PROJECT_ENV/bin/python $PROJECT_LOC/dns/dashboard.py %1 >> /var/log/yedor.net/godaddy-dns.log
+$PROJECT_ENV/bin/python $PROJECT_LOC/dns/godaddy-dns.py %1 >> /var/log/yedor.net/godaddy-dns.log
 
 #source name_Env/bin/activate
 ## virtualenv is now active.
