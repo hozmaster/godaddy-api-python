be15c40e3e200fd098a561ce7b15e5dbdf7ba358
Olli-Pekka Wallin <olli-pekka.wallin@yedor.net>
2018-09-22 14:55:10 +0300

    Set deploument script to work

--- deployment/godaddy_record_update.sh
+++ deployment/godaddy_record_update.sh
@@ -10,9 +10,7 @@ PYTHON_ENV=/usr/bin/python3
 #__prep_lock
 #exclusive_lock_now || exit 1
 
-PYTHON_ENV=/usr/bin/python3
-
-$PROJECT_ENV $PROJECT_LOC/dns/godaddy_record_update.py %1 >> /var/log/yedor.net/godaddy_record_update.log
+/usr/bin/python3 $PROJECT_LOC/dns/godaddy_record_update.py -f $1 >> /var/log/yedor.net/godaddy_record_update.log
 
 #source name_Env/bin/activate
 ## virtualenv is now active.
