f3efea0f68f0d7d008e24ab0b0e58c99e20d7f68
Olli-Pekka Wallin <opwallin@gmail.com>
2018-09-22 17:13:11 +0300

    Clean codebase

--- deployment/godaddy_record_update.sh
+++ deployment/godaddy_record_update.sh
@@ -5,14 +5,4 @@ set -eu
 PROJECT_LOC=/opt/godaddy-api-python
 PYTHON_ENV=/usr/bin/python3
 
-# . $PROJECT_LOC/locking
-
-#__prep_lock
-#exclusive_lock_now || exit 1
-
 /usr/bin/python3 $PROJECT_LOC/dns/godaddy_record_update.py -f $1 >> /var/log/yedor.net/godaddy_record_update.log
-
-#source name_Env/bin/activate
-## virtualenv is now active.
-##
-#/usr/bin/python3 godaddy-dns.py
